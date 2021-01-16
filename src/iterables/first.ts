9b6dbe6d34e5f50af18582cfef128cb05b7016aa
Anoop Naravaram <anoop901@gmail.com>
2021-01-16 16:12:39 -0600

    document first

--- src/iterables/first.ts
+++ src/iterables/first.ts
@@ -1,3 +1,10 @@
+/**
+ * Gets the first element of an iterable, if it is not empty.
+ *
+ * @typeparam T The type of element in the iterable.
+ * @param iterable The iterable.
+ * @return The first element of `iterable`, or `null` if the iterable is empty.
+ */
 export default function first<T>(iterable: Iterable<T>): T | null {
   for (const value of iterable) {
     return value;
