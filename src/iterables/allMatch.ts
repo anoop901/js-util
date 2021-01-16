1339362546b308290c6171dedfb42de3e753293a
Anoop Naravaram <anoop901@gmail.com>
2021-01-16 16:12:25 -0600

    document allMatch

--- src/iterables/allMatch.ts
+++ src/iterables/allMatch.ts
@@ -2,7 +2,18 @@ import chain from "../chain";
 import fold from "./fold";
 import map from "./map";
 
+/**
+ * Creates a function that checks if all values match a condition.
+ *
+ * @typeparam T The type of element in the iterable.
+ * @param pred The condition to check the elements with.
+ * @param pred.t The element to check.
+ */
 export default function allMatch<T>(pred: (t: T) => boolean) {
+  /**
+   * @param iterable The iterable whose elements to check.
+   * @return Whether all elements in the iterable match the condition.
+   */
   return (iterable: Iterable<T>): boolean => {
     return chain(iterable)
       .then(map(pred))
