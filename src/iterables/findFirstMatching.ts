13aec8c2ef63fcd8cd74eb5ee71172f7a7f4d112
Anoop Naravaram <anoop901@gmail.com>
2021-01-16 02:10:54 -0600

    Make findFirstMatching.ts depend on filter and first

--- src/iterables/findFirstMatching.ts
+++ src/iterables/findFirstMatching.ts
@@ -1,10 +1,8 @@
+import chain from "../chain";
+import filter from "./filter";
+import first from "./first";
+
 export default function findFirstMatching<T>(pred: (arg: T) => boolean) {
-  return (iterable: Iterable<T>): T | null => {
-    for (const value of iterable) {
-      if (pred(value)) {
-        return value;
-      }
-    }
-    return null;
-  };
+  return (iterable: Iterable<T>): T | null =>
+    chain(iterable).then(filter(pred)).then(first).end();
 }
